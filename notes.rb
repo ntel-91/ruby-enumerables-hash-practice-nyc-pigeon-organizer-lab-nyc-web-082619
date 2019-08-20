8b6b37b83be78bd997c4106c0e64a3d34b0f751f
Nick Telenson <ntelenson@gmail.com>
2019-08-20 21:54:12 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -17,23 +17,26 @@ pigeon_data = {
   }
 }
 
-new_hash = {}
-pigeon_data.each do |key, value|
-  value.each do |desc, names|
-    names.each do |bird_name|
-    
-      if !new_hash[bird_name]
-        new_hash[bird_name] = {}
-      end
-   
-      if !new_hash[bird_name][key]
-        new_hash[bird_name][key] = []
-      end
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  pigeon_data.each do |key, value|
+    value.each do |desc, names|
+      names.each do |bird_name|
       
-     new_hash[bird_name][key] << desc.to_s
+        if !new_hash[bird_name]
+          new_hash[bird_name] = {}
+        end
       
+        if !new_hash[bird_name][key]
+          new_hash[bird_name][key] = []
+        end
+        
+        new_hash[bird_name][key] << desc.to_s
+        
+      end
     end
   end
+  
 end
 
-
+p pigeon_data

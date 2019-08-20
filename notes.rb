673ee7a533b0d1b506d810789052120ee73e91bb
Nick Telenson <ntelenson@gmail.com>
2019-08-20 21:29:37 +0000

    Automatically backed up by Learn

---
+++ notes.rb
@@ -0,0 +1,31 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+pigeon_data.each do |key, value|
+  value.each do |desc, names|
+    names.each do |bird_name|
+      bird_name
+    
+      if  
+    
+    end
+  end
+end
+
+

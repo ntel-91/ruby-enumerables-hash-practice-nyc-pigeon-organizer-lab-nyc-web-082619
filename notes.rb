00f99f4032f74b8e2a0cd2ac24a8d17270155283
Nick Telenson <ntelenson@gmail.com>
2019-08-20 21:57:07 +0000

    Done.

---
+++ notes.rb
@@ -0,0 +1,43 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |desc, names|
+      names.each do |bird_name|
+      
+        if !new_hash[bird_name]
+          new_hash[bird_name] = {}
+        end
+      
+        if !new_hash[bird_name][key]
+          new_hash[bird_name][key] = []
+        end
+        
+        new_hash[bird_name][key] << desc.to_s
+        
+      end
+    end
+  end
+  new_hash
+end
+
+
+p nyc_pigeon_organizer(pigeon_data)
--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |desc, names|
+      names.each do |bird_name|
+      
+        if !new_hash[bird_name]
+          new_hash[bird_name] = {}
+        end
+      
+        if !new_hash[bird_name][key]
+          new_hash[bird_name][key] = []
+        end
+        
+        new_hash[bird_name][key] << desc.to_s
+        
+      end
+    end
+  end
+  new_hash
 end

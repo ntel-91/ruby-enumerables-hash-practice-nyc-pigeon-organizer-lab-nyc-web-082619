71f7cbc7093263630a509831c4ec1966b5964330
Nick Telenson <ntelenson@gmail.com>
2019-08-20 21:49:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,8 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
   new_hash = {}
   pigeon_data.each do |key, value|
     value.each do |desc, names|
       names.each do |bird_name|
-        bird_name
       
         if !new_hash[bird_name]
           new_hash[bird_name] = {}
